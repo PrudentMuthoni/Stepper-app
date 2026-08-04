e7c64cfe1201c35ce74317dc542903d799caced2
Prudent Muthoni <muthonimprudent@gmail.com>
2026-08-04 17:20:58 +0300

    initialized the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:stepper/screens/login_screen.dart';
-import 'package:stepper/screens/signup_screen.dart';
+import 'package:stepper/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +9,10 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SignupScreen());
+    return MaterialApp(
+    home: HomeScreen(),
+    theme: ThemeData(fontFamily: 'BlackRegular.ttf'
+    )
+    ,);
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() {
+    return _HomeScreenState();
+  }
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  String _tabTitle = "Home";
+  final List<Widget> _tabs = [_HomeTab(), _OrdersTab(), _ProfileTab()];
+  final List<String> _titles = ["Home", "Orders", "Profile"];
+
+  void _onTabClicked(int index){
+    setState(() {
+      _selectedIndex=index;
+      _tabTitle=_titles[index];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(_tabTitle)),
+      body: IndexedStack(index: _selectedIndex, children: _tabs),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor:  const Color.fromARGB(255, 223, 154, 212),
+        currentIndex: _selectedIndex,
+        onTap: _onTabClicked,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            activeIcon: Icon(Icons.shopping_bag),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_2_outlined),
+            activeIcon: Icon(Icons.person_2),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _HomeTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Home"));
+  }
+}
+
+class _OrdersTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Orders"));
+  }
+}
+
+class _ProfileTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Profile"));
+  }
+}
